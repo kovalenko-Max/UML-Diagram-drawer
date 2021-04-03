5b90f37597043115243c946a42c94b2d0af61a14
Rostislav <skochmen@gmail.com>
2021-04-04 00:33:57 +0300

    Added Class Pointer & methods drawPointer

--- UML Diagram drawer/Form1.Designer.cs
+++ UML Diagram drawer/Form1.Designer.cs
@@ -30,7 +30,6 @@ namespace UML_Diagram_drawer
         private void InitializeComponent()
         {
             this.pictureBoxMain = new System.Windows.Forms.PictureBox();
-            this.buttonCreateTableObject = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxMain)).BeginInit();
             this.SuspendLayout();
             // 
@@ -39,7 +38,7 @@ namespace UML_Diagram_drawer
             this.pictureBoxMain.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.pictureBoxMain.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBoxMain.BackColor = System.Drawing.Color.White;
             this.pictureBoxMain.Location = new System.Drawing.Point(12, 12);
             this.pictureBoxMain.Name = "pictureBoxMain";
             this.pictureBoxMain.Size = new System.Drawing.Size(960, 437);
@@ -48,22 +47,11 @@ namespace UML_Diagram_drawer
             this.pictureBoxMain.Click += new System.EventHandler(this.pictureBoxMain_Click);
             this.pictureBoxMain.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBoxMain_MouseDown);
             // 
-            // buttonCreateTableObject
-            // 
-            this.buttonCreateTableObject.Location = new System.Drawing.Point(768, 21);
-            this.buttonCreateTableObject.Name = "buttonCreateTableObject";
-            this.buttonCreateTableObject.Size = new System.Drawing.Size(189, 68);
-            this.buttonCreateTableObject.TabIndex = 1;
-            this.buttonCreateTableObject.Text = "Create";
-            this.buttonCreateTableObject.UseVisualStyleBackColor = true;
-            this.buttonCreateTableObject.MouseDown += new System.Windows.Forms.MouseEventHandler(this.buttonCreateTableObject_MouseDown);
-            // 
             // FormMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(984, 461);
-            this.Controls.Add(this.buttonCreateTableObject);
             this.Controls.Add(this.pictureBoxMain);
             this.Name = "FormMain";
             this.Text = "Form1";
@@ -76,7 +64,6 @@ namespace UML_Diagram_drawer
         #endregion
 
         private System.Windows.Forms.PictureBox pictureBoxMain;
-        private System.Windows.Forms.Button buttonCreateTableObject;
     }
 }
 
---
+++ UML Diagram drawer/Pointer.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UML_Diagram_drawer
+{
+    public class Pointer : Control
+    {
+        private PictureBox _pictureBox;
+        private Bitmap _bitmap;
+        private Graphics _graphics;
+        private Pen _pen;
+        //private Pen _eraser;
+        private const int _sizeOfArrow = 10;
+
+        public Point StartPoint { get; set; }
+        public Point EndPoint { get; set; }
+
+        public Pointer(PictureBox pictureBox, Bitmap bitMap, Graphics graphics, Pen pen)
+        {
+            _pictureBox = pictureBox;
+            _bitmap = bitMap;
+            _graphics = graphics;
+            _pen = pen;
+            //_eraser = new Pen(Color.White, 10f);
+        }
+
+        public void DrawSuccession(Point startP, Point endP, bool isHorizontal)
+        {
+            StartPoint = startP;
+            EndPoint = endP;
+
+            DrawStraightBrokenLine(startP, endP, isHorizontal);
+            DrawEndPointerSuccession(endP, isHorizontal);
+        }
+
+        public void DrawRealisation(Point startP, Point endP)
+        {
+            //DrawStraightBrokenLine(startP, endP);
+
+        }
+
+        public void DrawAssociation(Point startP, Point endP)
+        {
+            //DrawStraightBrokenLine(startP, endP);
+
+        }
+
+        public void DrawComposition(Point startP, Point endP)
+        {
+            //DrawStraightBrokenLine(startP, endP);
+
+        }
+
+        public void DrawAggregation(Point startP, Point endP)
+        {
+            //DrawStraightBrokenLine(startP, endP);
+
+        }
+
+        private void DrawStraightBrokenLine(Point startP, Point endP, bool isHorizontal)
+        {
+            if (isHorizontal)
+            {
+                Point[] points = new Point[]
+                {
+                    new Point(startP.X,startP.Y),
+                    new Point((endP.X + startP.X) / 2,startP.Y),
+                    new Point((endP.X + startP.X) / 2,endP.Y),
+                    new Point(endP.X,endP.Y)
+                };
+
+                _graphics.DrawLines(_pen, points);
+            }
+            else
+            {
+                Point[] points = new Point[]
+                {
+                    new Point(startP.X,startP.Y),
+                    new Point(endP.X, startP.Y),
+                    new Point(endP.X,endP.Y)
+                };
+
+                _graphics.DrawLines(_pen, points);
+            }
+        }
+
+        private void DrawEndPointerSuccession(Point endP, bool isHorizontal)
+        {
+            if (isHorizontal)
+            {
+                int coefX = StartPoint.X < endP.X ? endP.X - _sizeOfArrow : endP.X + _sizeOfArrow;
+                Point[] points = new Point[]
+                {
+                    new Point(coefX, endP.Y+_sizeOfArrow/2),
+                    new Point(coefX, endP.Y-_sizeOfArrow/2),
+                    new Point(endP.X, endP.Y)
+                };
+
+                Eraser(endP, _sizeOfArrow, isHorizontal);
+                _graphics.DrawPolygon(_pen, points);
+
+            }
+            else
+            {
+                int coefY = StartPoint.Y < endP.Y ? endP.Y - _sizeOfArrow : endP.Y + _sizeOfArrow;
+                Point[] points = new Point[]
+                {
+                    new Point(endP.X+_sizeOfArrow/2, coefY),
+                    new Point(endP.X-_sizeOfArrow/2, coefY),
+                    new Point(endP.X, endP.Y)
+                };
+
+                Eraser(endP, _sizeOfArrow, isHorizontal);
+                _graphics.DrawPolygon(_pen, points);
+            }
+        }
+
+        //private void DrawEndPointerSuccession(Point startP, Point endP)
+        //{
+        //    int offsetX = 10;
+        //    int offsetY = offsetX / 2;
+
+        //    _graphics.DrawLine(_pen, startP.X, startP.Y, endP.X - offsetX, endP.Y);
+        //    _graphics.DrawLine(_pen, endP.X - offsetX, endP.Y, endP.X - offsetX, endP.Y - offsetY);
+        //    _graphics.DrawLine(_pen, endP.X - offsetX, endP.Y - offsetY, endP.X, endP.Y);
+        //    _graphics.DrawLine(_pen, endP.X, endP.Y, endP.X - offsetX, endP.Y + offsetY);
+        //    _graphics.DrawLine(_pen, endP.X - offsetX, endP.Y + offsetY, endP.X - offsetX, endP.Y);
+        //}
+
+        private void DrawEndPointerRhombus(Point startP, Point endP)
+        {
+            int offsetX = 10;
+            int offsetY = offsetX - 2;
+
+            _graphics.DrawLine(_pen, startP.X, startP.Y, endP.X - offsetX * 2, endP.Y);
+            _graphics.DrawLine(_pen, endP.X - offsetX * 2, endP.Y, endP.X - offsetX, endP.Y - offsetY);
+            _graphics.DrawLine(_pen, endP.X - offsetX, endP.Y - offsetY, endP.X, endP.Y);
+            _graphics.DrawLine(_pen, endP.X, endP.Y, endP.X - offsetX, endP.Y + offsetY);
+            _graphics.DrawLine(_pen, endP.X - offsetX, endP.Y + offsetY, endP.X - offsetX * 2, endP.Y);
+        }
+
+        private void Eraser(Point point, int length, bool isHorizontal)
+        {
+            int coefX = isHorizontal ? length * (-1) : (int)_pen.Width * (-1);
+            int coefY = isHorizontal ? (int)_pen.Width * (-1) : length * (-1);
+            int coefI = StartPoint.X < point.X ? 1 : -1;
+            int coefJ = StartPoint.Y < point.Y ? 1 : -1;
+
+            for (int i = coefX; i < (int)_pen.Width; i++)
+            {
+                for (int j = coefY; j < (int)_pen.Width; j++)
+                {
+                    _bitmap.SetPixel((i * coefI) + point.X, (j * coefJ) + point.Y, _pictureBox.BackColor);
+                }
+            }
+        }
+    }
+}
